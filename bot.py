949bd60ce78a7b889e112557548afb853962fc3a
Lukas Short <las6731@rit.edu>
2020-10-09 01:09:32 -0400

    Read custom responses from json file

--- bot.py
+++ bot.py
@@ -1,19 +1,40 @@
 import discord
+import json
+import random
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 class LackBotClient(discord.Client):
+
+    responses: dict
+
     async def on_ready(self):
+        # load responses from json
+        with open('responses.json', 'r') as read_file:
+            self.responses = json.load(read_file)
+
+        # set activity
+        activity = discord.Game(name="Slackbot but worse")
+        await self.change_presence(activity=activity)
+
         print('Logged on as', self.user)
     
     async def on_message(self, message: discord.Message):
         # don't respond to ourselves
         if message.author == self.user:
             return
-        
-        if message.content == 'ping':
-            await message.channel.send('pong')
+
+        for phrase in self.responses:
+            if phrase in message.content.lower():
+                if isinstance(self.responses[phrase], str):
+                    await message.channel.send(self.responses[phrase])
+                    return
+                
+                options: list = self.responses[phrase]
+                response = random.randint(0, len(options)-1)
+                await message.channel.send(options[response])
+                return
 
 if __name__ == "__main__":
     client = LackBotClient()
