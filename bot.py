519016941a33fb4881515234ac0047cc08d51b73
Lukas Short <las6731@rit.edu>
2020-10-09 13:59:32 -0400

    It's wednesday, my dudes

--- bot.py
+++ bot.py
@@ -2,12 +2,16 @@ import discord
 import json
 import random
 import os
+from datetime import datetime
+from pytz import timezone
+
 from dotenv import load_dotenv
 load_dotenv()
 
 class LackBotClient(discord.Client):
 
     responses: dict
+    emojis = []
 
     async def on_ready(self):
         # load responses from json
@@ -18,15 +22,36 @@ class LackBotClient(discord.Client):
         activity = discord.Game(name="Slackbot but worse")
         await self.change_presence(activity=activity)
 
+        # discover emojis
+        for guild in self.guilds:
+            for emoji in guild.emojis:
+                self.emojis.append(emoji)
+
         print('Logged on as', self.user)
+
+    def find_emoji(self, name: str) -> discord.Emoji:
+        emoji: discord.Emoji
+        for emoji in self.emojis:
+            if emoji.name == name:
+                return emoji
+        return None
     
     async def on_message(self, message: discord.Message):
         # don't respond to ourselves
         if message.author == self.user:
             return
+        
+        msg = message.content.lower()
+        
+        if 'wednesday' in msg:
+            if datetime.now(tz=timezone('EST')).weekday() == 2:
+                await message.channel.send(f'{self.find_emoji("wednesday")} It\'s Wednesday, my dudes! {self.find_emoji("dab1")}{self.find_emoji("dab2")}{self.find_emoji("dab3yeet")}')
+            else:
+                await message.channel.send('It\'s not Wednesday yet :cry:')
+            return
 
         for phrase in self.responses:
-            if phrase in message.content.lower():
+            if phrase in msg:
                 if isinstance(self.responses[phrase], str):
                     await message.channel.send(self.responses[phrase])
                     return
